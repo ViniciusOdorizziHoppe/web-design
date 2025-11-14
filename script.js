f4914c93fcf22cc3a20742ac5f1d0fa1545bd0b5
VinciusOdorizziHoppe <viniciusodorizzi25@gmail.com>
2025-11-14 09:30:30 -0300

    incializando função de redicionamento

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function verGitHub() {
     window.location.href = "https://github.com/ViniciusOdorizziHoppe"
 }
+
+
+function Redicionar() {
+    
+var a = `Você Será direcionado em ${sec} segundos`
+var texto = document.getElementById("texto")
+
+set
+}
